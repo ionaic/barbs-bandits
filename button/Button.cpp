263875f759995aa29eeb86856a9abcd4ebd9f354
Ian Ooi <ionaic@gmail.com>
2012-10-22 11:34:32 -0400

    refactoring code to actually use subclasses

--- button/Button.cpp
+++ button/Button.cpp
@@ -1,18 +1,22 @@
+#pragma once
+
 #include <string>
-#include "../image/Image.h"
-#include "../text/Text.h"
+#include "Image.h"
+#include "Text.h"
 #include "Button.h"
+#include "Element.h"
 
 using namespace std;
 
-Button::Button(string content, int w, int h, int size, Image i) {
-	width = w;
-	height = h;
-	image = i;
-	text = new Text(width,height,size,content);
+Button::Button() {
+    Element::Element();
+}
+
+Button::Button(int x, int y, string content, int w, int h, int size, Image i) {
+    Element::Element(x, y, w, h);
+	Element::addChild(new Text(width,height,size,content));
 }
 
 Button::~Button() {
-	if (text) 
-		delete text;
+    Element::~Element();
 }
--- button/Button.h
+++ button/Button.h
@@ -1,19 +1,20 @@
+#pragma once
+
 #include <string>
-#include "../image/Image.h"
-#include "../text/Text.h"
+#include "Image.h"
+#include "Text.h"
+#include "Element.h"
 
 using namespace std;
 
-class Button {
+class Button : public Element {
 	public:
-		Button(string content, int w, int h, int size, Image image);
-		~Button();
-
-	private:
-		Text* text;
-		Image image;
-
-		int width;
-		int height;
-		
+        Button() : Element() {}
+        Button(int x, int y, int width, int height) : 
+            Element(x, y, width, height) {}
+		Button(int x, int y, int w, int h, string content, int size, Image* image) :
+            Element(x, y, w, h) {
+            Element::addChild(image);
+        }
+		~Button() { Element::~Element(); }
 };
