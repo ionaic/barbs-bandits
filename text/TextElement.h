6df4b61ebc639ab49e0c8dccefa2c56560bbb870
Ian Ooi <ionaic@gmail.com>
2012-10-25 21:12:25 -0400

    textelement

--- text/TextElement.h
+++ text/TextElement.h
@@ -6,5 +6,19 @@
 #include "Element.h"
 
 class TextElement : public Element {
+    public:
+        TextElement() : Element() {}
+        TextElement(unsigned int x, unsigned int y) : Element(x, y) {}
+        TextElement(unsigned int x, unsigned int y, Text* txt) { this->_text = txt; }
+        TextElement(unsigned int x, unsigned int y, unsigned int width, unsigned int height) : Element(x, y, width, height) {}
+        TextElement(unsigned int x, unsigned int y, unsigned int width, unsigned int height, Text * txt) { }
+        void setText(string text) {
+            this->_text = new Text(this->_width, this->_height, 10, text);
+        }
+        void setText(Text* text) {
+            this->_text = text;
+        }
 
-}
+    private: 
+        Text* _text;
+};
