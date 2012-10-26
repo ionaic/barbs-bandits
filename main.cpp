2772f0238c70f5d475843b5e8059113d5f534625
Ian Ooi <ionaic@gmail.com>
2012-10-26 10:52:22 -0400

    no longer segfaults if you do it dynamically allocated

--- main.cpp
+++ main.cpp
@@ -41,14 +41,16 @@ int loadGuiTexture(string textureString) {
 	T->render();
 	cout << T->stringify() << endl;
 	Image I(width,height,T->rendered);
-	unsigned char* bits = T->rendered;
+	//unsigned char* bits = T->rendered;
 
 	//For textures
-	//Image i(width, height, texturebits);
-	//ImageElement ie(0, 0, width, height, I);
+	Image i(width, height, texturebits);
+    ImageElement *ie2 = new ImageElement(5, 5, 20, 20, I);
+	ImageElement *ie = new ImageElement(0, 0, width, height, i);
 	//ImageElement ie2(0, 0, 50, 50, i2);
 	//ie.addChild(&ie2);
-	//Pixel* bits = ie.render()->getPixels();
+    ie->addChild(ie2);
+	Pixel* bits = ie->render()->getPixels();
 
 	if(!bits) {
 		cout << "Element texture failed to load" << endl;
