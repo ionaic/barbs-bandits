4b0fa4d827c6fd59c82e60833588db9df8e39f8d
andrewfasano <hello@andrewfasano.com>
2012-10-26 00:43:14 -0400

    Makefile now always makes clean and ImageElement no longer dynamically destructs things it shouldn't

--- image/ImageElement.h
+++ image/ImageElement.h
@@ -17,9 +17,6 @@ class ImageElement : public Element {
         ImageElement(unsigned int x, unsigned int y, unsigned int width, unsigned int height, Image *img) : Element(x, y, width, height) {
             this->_img = img;
         }
-        /*~ImageElement() { 
-            //delete _img;
-        }*/
         virtual void clearResult() {
             this->_img->blit(*(this->_result),  0U, 0U, 0U, 0U, this->_width, this->_height);
         }
