af53e7ff42f5a11e8b0c7a978d87f7fded5eac7c
Ian Ooi <ionaic@gmail.com>
2012-10-28 19:09:05 -0400

    conflictslol

--- text/TextElement.h
+++ text/TextElement.h
@@ -15,9 +15,11 @@ class TextElement : public Element {
         TextElement(unsigned int x, unsigned int y, unsigned int width, unsigned int height, Text &txt) { this->_text = new Text(txt); }
         ~TextElement() { delete _text; }
         void setText(string text) {
+            delete _text;
             this->_text = new Text(this->_width, this->_height, 10, text);
         }
         void setText(Text& text) {
+            delete _text;
             this->_text = new Text(text);
         }
 
