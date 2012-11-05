ec7dd0824b7157971d703377a10e802f79440d60
andrewfasano <hello@andrewfasano.com>
2012-11-05 00:27:58 -0500

    Added fractional counter cpp and button might do dynamic font sizes now

--- button/Button.cpp
+++ button/Button.cpp
@@ -5,6 +5,7 @@
 #include "../image/ImageElement.h"
 #include "../text/TextElement.h"
 #include "Button.h"
+#include <iostream>
 
 /*!Default constructor creates an empty instance of the class. */
 Button::Button() : Element() {}
@@ -23,7 +24,15 @@ Button::Button(unsigned int x, unsigned int y, unsigned int width,
 /*!Constructor sets x,y, width, heigh and text */
 Button::Button(unsigned int x, unsigned int y, unsigned int width,
 		unsigned int height, string txt) : Element(x, y, width, height) {
-	unsigned int size = 14; //TODO: do math based on width and height to determine size
+	int h = height>>1;
+	int w = (width/txt.size()<<1);
+	unsigned int size;
+	if (h > w) {
+		size = h;
+	}else{
+		size = w;
+	}
+
 	_imageE = new ImageElement(0, 0, width, height); //need to draw image first
 	addChild(_imageE);
 	_textE = new TextElement(0, 0, width, height, size, txt);
---
+++ counter/FractionalCounter.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <sstream>
+#include "../image/Image.h"
+#include "../text/Text.h"
+#include "../element/Element.h"
+#include "../image/ImageElement.h"
+#include "../text/TextElement.h"
+#include "FractionalCounter.h"
+
+using namespace std;
+
+FractionalCounter::FractionalCounter() {
+	FractionalCounter(0,0,0,0,0);
+}
+
+FractionalCounter::FractionalCounter(int x, int y, int width, int height, int value) : BoundedCounter(x,y,width,height,value) {
+	string s1 = static_cast<ostringstream*>( &(ostringstream() << value) )->str();
+	string s2 = static_cast<ostringstream*>( &(ostringstream() << _max) )->str();
+	string result = s1 + "/" + s2;
+	_textE = new TextElement(x,y,width, height, 14, result);
+	addChild(_textE);
+	render();
+}
+
+FractionalCounter::~FractionalCounter() {
+	delete textE;
+}
