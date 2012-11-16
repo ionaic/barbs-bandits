934028d846d03b39e48698e21259f706f7c099e4
Matthew McMullan <nerdfencer@gmail.com>
2012-11-16 11:05:30 -0500

    Sex

---
+++ openGUI.h
@@ -0,0 +1,19 @@
+#ifndef __libOpenGUI_h
+#define __libOpenGUI_h
+
+#include "libOpenGUI/ToggleButton.h"
+#include "libOpenGUI/Image.h"
+#include "libOpenGUI/ImageElement.h"
+#include "libOpenGUI/Pixel.h"
+#include "libOpenGUI/Element.h"
+#include "libOpenGUI/AbstractCounter.h"
+#include "libOpenGUI/BoundedCounter.h"
+#include "libOpenGUI/ProgressBar.h"
+#include "libOpenGUI/NumericCounter.h"
+#include "libOpenGUI/FractionalCounter.h"
+#include "libOpenGUI/SliderBar.h"
+#include "libOpenGUI/CheckBox.h"
+#include "libOpenGUI/RadioButton.h"
+#include "libOpenGUI/Button.h"
+#endif
+
