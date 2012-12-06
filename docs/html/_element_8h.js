5355397d8b869e926d8abbf6b40c3d62eead3218
andrewfasano <hello@andrewfasano.com>
2012-12-05 21:32:23 -0500

    Updated doxygen

--- docs/html/_element_8h.js
+++ docs/html/_element_8h.js
@@ -1,4 +1,6 @@
 var _element_8h =
 [
+    [ "Element", "class_element.html", "class_element" ],
+    [ "ElementComparison", "class_element_comparison.html", "class_element_comparison" ],
     [ "currentId", "_element_8h.html#a1079e235504c602863bffffdf3dfc200", null ]
 ];
